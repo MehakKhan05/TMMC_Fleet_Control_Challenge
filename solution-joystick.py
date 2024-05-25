5e8ccb91462dc491167ec379eb121d2457cedd4a
abgros <58993004+abgros@users.noreply.github.com>
2024-05-25 17:23:36 -0400

    Update solution-joystick.py

--- solution-joystick.py
+++ solution-joystick.py
@@ -1,10 +1,13 @@
 import TMMC_Wrapper
 import rclpy
 from pynput.keyboard import Listener
-
-USING_HARDWARE = False
+import numpy as np
+from PIL import Image
+import os
+import json
 
 # initialization
+print("Initializing...")
 TMMC_Wrapper.is_SIM = True
 if not rclpy.ok():
 	rclpy.init()
@@ -20,8 +23,6 @@ class Robot:
 			velocity = 0.0
 		else:
 			velocity = min(0.8 * self.min_dist_front, 0.5)
-		if USING_HARDWARE:
-			velocity /= 2
 		self.bot.send_cmd_vel(velocity, 0.0)
 
 	def move_backward(self):
@@ -29,15 +30,13 @@ class Robot:
 			velocity = 0.0
 		else:
 			velocity = min(0.8 * self.min_dist_back, 0.5)
-		if USING_HARDWARE:
-			velocity /= 2
 		self.bot.send_cmd_vel(-velocity, 0.0)
 
 	def turn_left(self):
-		self.bot.send_cmd_vel(0.0, 0.25)
+		self.bot.send_cmd_vel(0.0, 0.5)
 
 	def turn_right(self):
-		self.bot.send_cmd_vel(0.0, -0.25)
+		self.bot.send_cmd_vel(0.0, -0.5)
 
 	def stop_robot(self):
 		self.bot.send_cmd_vel(0.0, 0.0)
@@ -64,23 +63,21 @@ class Robot:
 			self.stop_robot()
 		
 		try:
-			if USING_HARDWARE:
-				self.min_dist_front = sorted(self.bot.last_scan_msg.ranges[90:270])[2] / 1.5 # get third smallest
-			else:
+			if TMMC_Wrapper.is_SIM:
 				self.min_dist_front = min(self.bot.last_scan_msg.ranges[0:45] + self.bot.last_scan_msg.ranges[315:360])
+			else:
+				self.min_dist_front = sorted(self.bot.last_scan_msg.ranges[90:270])[2] # get third smallest
 		except:
 			self.min_dist_front = -1
 		
 		try:
-			if USING_HARDWARE:
-				self.min_dist_back = sorted(self.bot.last_scan_msg.ranges[450:630])[2] / 1.5 # get third smallest
-			else:
+			if TMMC_Wrapper.is_SIM:		
 				self.min_dist_back = min(self.bot.last_scan_msg.ranges[135:225])
+			else:
+				self.min_dist_back = sorted(self.bot.last_scan_msg.ranges[450:630])[2] # get third smallest
 		except:
 			self.min_dist_back = -1
-		
-		print("FRONT DISTANCE:", self.min_dist_front, "BACK DISTANCE:", self.min_dist_back)
-	
+			
 		if "w" in self.keys_pressed:
 			self.move_forward()
 		if "s" in self.keys_pressed:
@@ -89,15 +86,36 @@ class Robot:
 			self.turn_left()
 		if "d" in self.keys_pressed:
 			self.turn_right()
+		
+		if "i" in self.keys_pressed:
+			self.keys_pressed = set()
+			print("FRONT DISTANCE:", self.min_dist_front, "BACK DISTANCE:", self.min_dist_back)
+
+		if "l" in self.keys_pressed:
+			self.keys_pressed = set()
+			print([round(min(10, i), 3) for i in self.bot.last_scan_msg.ranges])
+		
+		if "p" in self.keys_pressed:
+			self.keys_pressed = set()
+			try:
+				img = self.bot.last_image_msg
+				img_array = np.reshape(img.data, (img.height, img.width, 3))
+
+				PIL_image = Image.fromarray(img_array.astype("uint8"), "RGB")
+				PIL_image.save(os.path.join(os.path.expanduser('~'), 'robot_image.png'))
+			except:
+				pass
 
 		rclpy.spin_once(self.bot, timeout_sec=0.1)
+
+	def shutdown(self):
+		self.bot.destroy_node()
 
 ROBOT1 = Robot()
 
 def main():
-	print("running main")
 	ROBOT1.safe_keyboard_control()
-	print("Listening for keyboard events. Press keys to test, Ctrl C to exit")
+	print("Listening for keyboard events.")
 	while True:
 		ROBOT1.eventloop()
 		
@@ -106,7 +124,5 @@ try:
 except KeyboardInterrupt:
 	print("Keyboard interrupt receieved. Stopping...")
 finally:
-	#when exiting program, run the kill processes
-	ROBOT1.bot.stop_keyboard_control()
-	ROBOT1.bot.destroy_node()
+	ROBOT1.shutdown()
 	rclpy.shutdown()
